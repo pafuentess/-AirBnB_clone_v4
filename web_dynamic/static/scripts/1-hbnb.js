f9dba52b77a559d9fe196f02b11d552f33f029ed
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-05-15 20:29:21 +0000

    Added task 2 checkbox

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,18 @@
-document.ready(function () {
+$(document).ready(function () {
   const amenities = {};
-  $('input[type="checkbox']).click(function () {
-    if ($(this).prop('checked') == true) {
+  $('input[type="checkbox"]').click(function () {
+    if ($(this).prop('checked') === true) {
       amenities[$(this).data('name')] = $(this).data('id');
-    } else if ($(this).prop('checked') == false) {
+    } else if ($(this).prop('checked') === false) {
       delete amenities[$(this).data('name')];
     }
     console.log(amenities);
+    const H4 = $(this).closest('.amenities').find('h4');
+    const list1 = [];
+    for (const i in amenities) {
+      list1.push(i);
+    }
+    console.log(list1);
+    H4.text(list1);
   });
 });
