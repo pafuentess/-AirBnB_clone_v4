85d8039e317a1f1494cebe772d08407ff487c1ad
pafuentess <pafuentess@unal.edu.co>
2020-05-15 20:44:12 -0500

    fix task 3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -17,9 +17,10 @@ $(document).ready(function () {
   });
   $.get('http://0.0.0.0:5001/api/v1/status/', function(data, Status) {
     if (data.status === 'OK') {
-      $('DIV#api_status').addClass('available');
+      $('#api_status').addClass('available');
     } else {
-      $('DIV#api_status').removeClass('available');
+      $('#api_status').removeClass('available');
+      $('#api_status').addClass('noavailable');
     }
   });
 });
